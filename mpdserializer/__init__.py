2475cb206da7b6466a4c66ff5804a4133e773a45
Dugan Chen <thedoogster@gmail.com>
2014-02-10 13:02:40 -0800

    mpdserializer is its own package

---
+++ mpdserializer/__init__.py
@@ -0,0 +1,23 @@
+from .mpdserializer import CommandError
+assert CommandError
+
+from .mpdserializer import ConnectionError
+assert ConnectionError
+
+from .mpdserializer import ProtocolError
+assert ProtocolError
+
+from .mpdserializer import deserialize_hello
+assert deserialize_hello
+
+from .mpdserializer import deserialize_nothing
+assert deserialize_nothing
+
+from .mpdserializer import deserialize_tuple
+assert deserialize_tuple
+
+from .mpdserializer import deserialize_dict
+assert deserialize_dict
+
+from .mpdserializer import deserialize_songs
+assert deserialize_songs
